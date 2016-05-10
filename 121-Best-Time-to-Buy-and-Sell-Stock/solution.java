00849ced1c463c065732318e06f6cc7e0944adff
Charliebob <cxz131330@utdallas.edu>
2016-05-10 10:30:28 -0700

    [121. Best Time to Buy and Sell Stock][Accepted]committed by LeetCode Extension

--- 121-Best-Time-to-Buy-and-Sell-Stock/solution.java
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.java
@@ -1,9 +1,9 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        int min = Integer.MAX_VALUE; //define the max as min 
-        int max = 0;                 //define the min as max
-        for(int p: prices){
-            min = Math.min(min, p);
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        for(int p:prices){
+            min = Math.min(p, min);
             max = Math.max(p-min, max);
         }
         return max;
