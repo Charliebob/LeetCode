0627d6b5a886a713fdbec2d81a159fc95ca5e78b
Charliebob <cxz131330@utdallas.edu>
2016-05-13 11:00:54 -0700

    [232. Implement Queue using Stacks][Runtime Error]committed by LeetCode Extension

---
+++ 232-Implement-Queue-using-Stacks/solution.java
@@ -0,0 +1,26 @@
+class MyQueue {
+    // Push element x to the back of queue.
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+    public void push(int x) {
+        stack1.push(x);
+        while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        stack2.pop();
+    }
+
+    // Get the front element.
+    public int peek() {
+        return stack2.pop();
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        return stack1.isEmpty()&&stack2.isEmpty();
+    }
+}
