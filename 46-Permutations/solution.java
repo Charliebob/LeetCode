bfc286f61da0767dfe4b4ad903aa3964353f4132
Charliebob <cxz131330@utdallas.edu>
2016-05-15 23:54:41 -0700

    [46. Permutations][Accepted]committed by LeetCode Extension

--- 46-Permutations/solution.java
+++ 46-Permutations/solution.java
@@ -1,23 +1,29 @@
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        //if nums array length is 0 return empty list
-        if(nums.length==0) return result;
-        //define an inside list
-        List<Integer> l0 = new ArrayList<Integer>();
-        l0.add(nums[0]);
-        result.add(l0);
-        for(int i=1; i<nums.length; ++i){
-            List<List<Integer>> new_result = new ArrayList<List<Integer>>();
-            for(int j=0; j<=i; ++j){
-                for(List<Integer>l: result){
-                    List<Integer> new_l = new ArrayList<Integer>(l);
-                    new_l.add(j, nums[i]);
-                    new_result.add(new_l);
-                }
-            }
-            result = new_result;
+    perm(result,nums,0,nums.length-1);
+    return result;
+}
+public static void perm(List<List<Integer>> result, int[] nums, int start, int end){
+    if(start==end){
+        Integer[] ele = new Integer[nums.length];
+        for(int i=0; i<nums.length; i++){
+            ele[i] = nums[i];
         }
-        return result;
+        result.add(Arrays.asList(ele));
+    }
+    else{
+        for(int i=start; i<=end; i++){
+            int temp = nums[start];
+            nums[start] = nums[i];
+            nums[i] = temp;
+
+            perm(result, nums,start+1,end);
+
+            temp = nums[start];
+            nums[start] = nums[i];
+            nums[i] = temp;
+        }
+    }
     }
 }
