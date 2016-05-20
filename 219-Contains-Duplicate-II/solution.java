80b461e429ff033c09c396df2bf7432b32544081
Charliebob <cxz131330@utdallas.edu>
2016-05-20 14:17:43 -0700

    [219. Contains Duplicate II][Accepted]committed by LeetCode Extension

--- 219-Contains-Duplicate-II/solution.java
+++ 219-Contains-Duplicate-II/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashSet<Integer> hs = new HashSet<Integer>();
         for(int i=0; i<nums.length; i++){
-            if(i>k) hs.remove(nums[i-k-i]);
+            if(i>k) hs.remove(nums[i-k-1]);
             if(!hs.add(nums[i])) return true;
         }
         return false;
