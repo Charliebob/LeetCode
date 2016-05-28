a5304d0a8a200a94a645003b6c9b5c97360057f0
Charliebob <cxz131330@utdallas.edu>
2016-05-28 15:49:10 -0700

    [62. Unique Paths][Accepted]committed by LeetCode Extension

--- 62-Unique-Paths/solution.java
+++ 62-Unique-Paths/solution.java
@@ -4,8 +4,8 @@ public class Solution {
         for(int i=0; i<m; i++){
             step[i][0] = 1;
         }
-        for(int i=0; i<n; i++){
-            step[0][i] = 1;
+        for(int j=0; j<n; j++){
+            step[0][j] = 1;
         }
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
