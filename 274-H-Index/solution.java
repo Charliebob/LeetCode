a2ff1bf5566782485c0f73625333db6e192030ff
Charliebob <cxz131330@utdallas.edu>
2016-06-07 11:19:08 -0700

    [274. H-Index][Accepted]committed by LeetCode Extension

--- 274-H-Index/solution.java
+++ 274-H-Index/solution.java
@@ -1,18 +1,16 @@
 public class Solution {
     public int hIndex(int[] citations) {
         if(citations==null || citations.length==0) return 0;
-        Arrays.sort(citations);
-        int left = 0;
-        int right = citations.length-1;
-        int mid = left + (right-left)/2;
-        while(left<right){
-            if((citations[mid]>mid-1)&&(citations[mid]<=right-mid+1)){
-                return mid;
-            }else if(citations[mid]<mid-1){
-                mid++;
-            }else{
-                mid--;
-            }
+        int len = citations.length;
+        int[] count = new int[len+1];
+        for(int c:citations){
+            if(c>len) count[len]++;
+            else count[c]++;
+        }
+        int total = 0;
+        for(int i=len; i>=0; i--){
+            total+=count[i];
+            if(total>=i) return i;
         }
         return 0;
     }
