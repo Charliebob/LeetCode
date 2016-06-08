a2aa0f0965f821b8c09e68ec81c6b02a495aee94
Charliebob <cxz131330@utdallas.edu>
2016-06-08 11:33:10 -0700

    [39. Combination Sum][Compile Error]committed by LeetCode Extension

--- 39-Combination-Sum/solution.java
+++ 39-Combination-Sum/solution.java
@@ -5,11 +5,11 @@ public class Solution {
         getResult(result, new ArrayList<Integer>(), candidates, target, 0);
         return result;
     }
-    private void getResult(List<List<Integer>>result, List<Integer>cur, int candidates[], int target, int start){
+    private void getResult(List<List<Integer>> result, List<Integer>cur, int candidates[], int target, int start){
         if(target>0){
-            for(int i=start; i<candidates.length && target>=candidates[i]; i++){
+            for(int i=start; i<candidates.length && target>=candidates[i];i++){
                 cur.add(candidates[i]);
-                getResult(result, cur,candidates, target-candidates[i],i);
+                get(result, cur, candidates, target-candidates[i], i);
                 cur.remove(cur.size()-1);
             }
         }else if(target==0){
