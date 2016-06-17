26544bc8705d00020e3052e2cdb446b9b38a2e09
Charliebob <cxz131330@utdallas.edu>
2016-06-17 16:49:13 -0700

    [163. Missing Ranges][Accepted]committed by LeetCode Extension

--- 163-Missing-Ranges/solution.java
+++ 163-Missing-Ranges/solution.java
@@ -1,31 +1,15 @@
 public class Solution {
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
-        List<String> result = new ArrayList<String>();
-        if(nums==null) return result;
-        if(nums.length==0){
-            if(lower==upper){
-                result.add(Integer.toString(lower));
-            }else{
-                result.add(lower+"->"+upper);
-            }
-            return result;
+        List<String> list = new ArrayList<String>();
+        for(int n : nums){
+            int justBelow = n - 1;
+            if(lower == justBelow) list.add(lower+"");
+            else if(lower < justBelow) list.add(lower + "->" + justBelow);
+            lower = n+1;
         }
-        if(lower+2<=nums[0]) result.add(lower+"->"+(nums[0]-1));
-        else if(lower+2==nums[0]) result.add(Integer.toString(lower+1));
-        else if(lower+1==nums[0]) result.add(Integer.toString(lower));
-        for(int i=1; i<nums.length; i++){
-            if(nums[i-1]+1==nums[i]||nums[i-1]==nums[i]) continue;
-            else if(nums[i-1]+2==nums[i]){
-                result.add(Integer.toString(nums[i-1]+1));
-            }else if(nums[i-1]+2<nums[i]){
-                result.add(nums[i-1]+1 +"->"+(nums[i]-1));
-            }
-        }
-        if(nums[nums.length-1]+2<=upper){
-            result.add(nums[nums.length-1]+1 + "->" + upper);
-        }else if(nums[nums.length-1]+1==upper){
-            result.add(Integer.toString(upper));
-        }
-        return result;
+        if(lower == upper) list.add(lower+"");
+        else if(lower < upper) list.add(lower + "->" + upper);
+        return list;
     }
 }
+
