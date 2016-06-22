15f5f1adfa4a492ebf9ba2fab4dc7719603bc816
Charliebob <cxz131330@utdallas.edu>
2016-06-22 15:44:11 -0700

    [70. Climbing Stairs][Accepted]committed by LeetCode Extension

--- 70-Climbing-Stairs/solution.java
+++ 70-Climbing-Stairs/solution.java
@@ -1,12 +1,12 @@
 public class Solution {
     public int climbStairs(int n) {
-        int[] tmp = new int[n];
+        int[] tmp = new int[n+1];
         if (n < 2) return 1;
         tmp[0] = 1;
-        tmp[1] = 2;
-        for (int i = 2; i < n; i++){
+        tmp[1] = 1;
+        for (int i = 2; i < n+1; i++){
             tmp[i] = tmp[i-1] + tmp[i-2];
         }
-        return tmp[n-1];
+        return tmp[n];
     }
 }
