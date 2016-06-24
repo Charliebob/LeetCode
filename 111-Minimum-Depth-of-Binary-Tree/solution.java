50f51c84fd1eed8977806f839e8321b78e7cf702
Charliebob <cxz131330@utdallas.edu>
2016-06-24 11:42:12 -0700

    [111. Minimum Depth of Binary Tree][Accepted]committed by LeetCode Extension

--- 111-Minimum-Depth-of-Binary-Tree/solution.java
+++ 111-Minimum-Depth-of-Binary-Tree/solution.java
@@ -9,28 +9,26 @@
  */
 public class Solution {
     public int minDepth(TreeNode root) {
-        if (root == null){
-        return 0;
-    }
-    Queue<TreeNode> queue = new LinkedList<TreeNode>();
-    queue.add(root);
-    int depth = 1;
-    while (!queue.isEmpty()){
-        int size = queue.size(); // determine the loop size
-        for (int i = 0; i< size; i++){
-            TreeNode node = queue.poll();
-            if (node.left == null && node.right == null){
-                return depth;
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int depth = 1;
+        while (!queue.isEmpty()){
+            int size = queue.size(); // determine the loop size
+            for (int i = 0; i< size; i++){
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null){
+                    return depth;
+                }
+                if (node.left!=null){
+                    queue.add(node.left);
+                }
+                if (node.right!=null){
+                    queue.add(node.right);
+                }
             }
-            if (node.left!=null){
-                queue.add(node.left);
-            }
-            if (node.right!=null){
-                queue.add(node.right);
-            }
+            depth ++;
         }
-        depth ++;
-    }
-    return depth;
+        return depth;
     }
 }
