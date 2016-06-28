ec5cbd459afcbf40ac75db1a9aaeac104b1d9b55
Charliebob <cxz131330@utdallas.edu>
2016-06-28 15:36:06 -0700

    [239. Sliding Window Maximum][Compile Error]committed by LeetCode Extension

--- 239-Sliding-Window-Maximum/solution.java
+++ 239-Sliding-Window-Maximum/solution.java
@@ -1,14 +1,28 @@
 public class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if(nums.length==0) return new int[0];
-        Deque<Integer> deque = new LinkedList<Integer>();
-        int[] res = new int[nums.length-k+1];
-        for(int i=0;i<nums.length;i++){
-            if(i>=k && deque.getLast()==nums[i-k]) deque.removeLast();  
-            while(!deque.isEmpty() && deque.getFirst()<nums[i]) deque.removeFirst();
-            deque.addFirst(nums[i]);
-            if(i+1>=k) res[i-k+1] = deque.getLast();
+        if (a == null || k <= 0) {
+            return new int[0];
         }
-        return res;
+        int n = a.length;
+        int[] r = new int[n-k+1];
+        int ri = 0;
+        // store index
+        Deque<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < a.length; i++) {
+            // remove numbers out of range k
+            while (!q.isEmpty() && q.peek() < i - k + 1) {
+                q.poll();
+            }
+            // remove smaller numbers in k range as they are useless
+            while (!q.isEmpty() && a[q.peekLast()] < a[i]) {
+                q.pollLast();
+            }
+            // q contains index... r contains content
+            q.offer(i);
+            if (i >= k - 1) {
+                r[ri++] = a[q.peek()];
+            }
+        }
+        return r;
     }
 }
