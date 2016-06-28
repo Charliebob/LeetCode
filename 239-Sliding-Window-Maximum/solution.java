912084cb970c59846d09345042babfc9b3d6d1da
Charliebob <cxz131330@utdallas.edu>
2016-06-28 15:39:27 -0700

    [239. Sliding Window Maximum][Accepted]committed by LeetCode Extension

--- 239-Sliding-Window-Maximum/solution.java
+++ 239-Sliding-Window-Maximum/solution.java
@@ -1,28 +1,25 @@
 public class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums == null || k <= 0) {
-            return new int[0];
+        int len = nums.length;
+    int[] result = new int[len - k + 1];
+    if(nums.length == 0) return new int[0];
+    Queue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>(){
+        @Override
+        public int compare(Integer i1, Integer i2){
+            return Integer.compare(i2, i1);
         }
-        int n = nums.length;
-        int[] r = new int[n-k+1];
-        int ri = 0;
-        // store index
-        Deque<Integer> q = new ArrayDeque<>();
-        for (int i = 0; i < nums.length; i++) {
-            // remove numbers out of range k
-            while (!q.isEmpty() && q.peek() < i - k + 1) {
-                q.poll();
-            }
-            // remove smaller numbers in k range as they are useless
-            while (!q.isEmpty() && nums[q.peekLast()] < nums[i]) {
-                q.pollLast();
-            }
-            // q contains index... r contains content
-            q.offer(i);
-            if (i >= k - 1) {
-                r[ri++] = nums[q.peek()];
-            }
-        }
-        return r;
+    });
+
+    for(int i = 0; i < k; i ++){
+        queue.add(nums[i]);
+    }
+    result[0] = queue.peek();
+    for(int i = k; i < len; i ++){
+        queue.remove(nums[i - k]);
+        queue.add(nums[i]);
+        result[i - k + 1] = queue.peek();
+    }
+
+    return result;
     }
 }
