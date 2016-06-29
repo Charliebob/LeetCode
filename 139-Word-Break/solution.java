8fc4c38b78cd01bdfb332b7b7483c35a650a6ee9
Charliebob <cxz131330@utdallas.edu>
2016-06-29 16:40:37 -0700

    [139. Word Break][Compile Error]committed by LeetCode Extension

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -1,22 +1,15 @@
 public class Solution {
     public boolean wordBreak(String s, Set<String> wordDict) {
-        int n = s.length();
-        boolean[] canWordBreak = new boolean[n + 1];
-        int minLength = Integer.MAX_VALUE;
-        int maxLength = Integer.MIN_VALUE;
-        for(String str : wordDict) {
-            minLength = Math.min(minLength, str.length());
-            maxLength = Math.max(maxLength, str.length());
-        }
-        canWordBreak[0] = true;
-        for(int i = minLength; i <= n; i++) {
-            for(int j = i - minLength; j >= 0 && j >= i - maxLength; j--) {
-                if(canWordBreak[j] && wordDict.contains(s.substring(j, i))) {
-                    canWordBreak[i] = true;
+        boolean[] f = new boolean[s.length() + 1];
+        f[0] = true;
+        for(int i=1; i <= s.length(); i++){
+            for(int j=0; j < i; j++){
+                if(f[j] && dict.contains(s.substring(j, i))){
+                    f[i] = true;
                     break;
                 }
             }
         }
-        return canWordBreak[n];
+        return f[s.length()];
     }
 }
