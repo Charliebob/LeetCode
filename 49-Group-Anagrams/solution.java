c0aa2c5c2eb0bb124b300a3fe45beb9c1973424d
Charliebob <cxz131330@utdallas.edu>
2016-06-29 17:32:42 -0700

    [49. Group Anagrams][Accepted]committed by LeetCode Extension

---
+++ 49-Group-Anagrams/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        Map<String, Integer> map = new HashMap<String, Integer>(); //for each anagram word saves the index
+        Arrays.sort(strs);
+        for(String str: strs){
+            char[] c = str.toCharArray();
+            Arrays.sort(c);
+            String string = new String(c);
+            if(map.containsKey(string)){
+                List<String> li = result.get(map.get(string));
+                li.add(str);
+            }else{
+                List<String> li = new ArrayList<String>();
+                li.add(str);
+                result.add(li);
+                map.put(string, result.size()-1);
+            }
+        }
+        return result; 
+    }
+}
