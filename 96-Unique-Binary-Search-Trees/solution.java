910da5ae620ae100141d6b93963f7d35aeadb2ee
Charliebob <cxz131330@utdallas.edu>
2016-07-07 10:32:56 -0700

    [96. Unique Binary Search Trees][Accepted]committed by LeetCode Extension

--- 96-Unique-Binary-Search-Trees/solution.java
+++ 96-Unique-Binary-Search-Trees/solution.java
@@ -4,11 +4,9 @@ public class Solution {
         tree[0] = 1;
         tree[1] = 1;
         for(int i=2; i<=n; i++){
-            //int sum = 0;
             for(int j=0; j<i; j++){
                 tree[i]+=tree[j]*tree[i-j-1];
             }
-            //tree[i] = sum;
         }
         return tree[n];
     }
