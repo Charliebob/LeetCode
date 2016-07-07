bb8079fd4084816446b01524b47086cefd12bd7c
Charliebob <cxz131330@utdallas.edu>
2016-07-07 16:49:00 -0700

    [364. Nested List Weight Sum II][Accepted]committed by LeetCode Extension

--- 364-Nested-List-Weight-Sum-II/solution.java
+++ 364-Nested-List-Weight-Sum-II/solution.java
@@ -18,18 +18,17 @@
 public class Solution {
     public int depthSumInverse(List<NestedInteger> nestedList) {
         int unweighted = 0, weighted = 0;
-    while (!nestedList.isEmpty()) {
-        List<NestedInteger> nextLevel = new ArrayList<>();
-        for (NestedInteger ni : nestedList) {
-            if (ni.isInteger())
-                unweighted += ni.getInteger();
-            else
-                nextLevel.addAll(ni.getList());
+        while (!nestedList.isEmpty()) {
+            List<NestedInteger> nextLevel = new ArrayList<>();
+            for (NestedInteger ni : nestedList) {
+                if (ni.isInteger())
+                    unweighted += ni.getInteger();
+                else
+                    nextLevel.addAll(ni.getList());
+            }
+            weighted += unweighted;
+            nestedList = nextLevel;
         }
-        weighted += unweighted;
-        nestedList = nextLevel;
-    }
-    return weighted;
-    
+        return weighted;
     }
 }
