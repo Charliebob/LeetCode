1eaae7e0191eb68c88e602b0c0244d9fc6b03086
Charliebob <cxz131330@utdallas.edu>
2016-07-09 18:10:31 -0700

    [372. Super Pow][Compile Error]committed by LeetCode Extension

--- 372-Super-Pow/solution.java
+++ 372-Super-Pow/solution.java
@@ -1,22 +1,26 @@
 public class Solution {
     public int superPow(int a, int[] b) {
-        int result = 1;
-        while (b != 0) {
-            if (b % 2 != 0)
-                result = result * a % M;
-            a = a * a % M;
-            b /= 2;
+        long mod = (long) a;
+        final long div = 1337L;
+        final List<Integer> list = new ArrayList<>();
+        final Set<Integer> used = new HashSet<>();
+        int key;
+        while (true) {
+            mod %= div;
+            key = (int) mod;
+            // Is the list starting to repeat?
+            if (used.contains(key)) break;
+            list.add(key);
+            used.add(key);
+            mod *= a;
         }
-        return result;
-    }
-
-    public int superPow(int a, int[] b) {
-        a %= M;
-        int result = 1;
-        for (int i = b.length - 1; i >= 0; i--) {
-            result = result * normalPow(a, b[i]) % M;
-            a = normalPow(a, 10);
-        }
-        return result;
-    }
+        // Find the actual mod value from the list
+        // BigInteger was used below to simplify (b mod list_size)
+        // since this problem wasn't about large number division
+        final StringBuilder sb = new StringBuilder();
+        for (int d : b) sb.append(d);
+        key = new BigInteger(sb.toString())
+                .mod(BigInteger.valueOf(list.size())).intValue();
+        if (key == 0) key = list.size();
+        return list.get(key - 1);
 }
