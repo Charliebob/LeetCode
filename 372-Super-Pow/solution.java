09dfd0d583723fa5679d781018ef34edf2e6079d
Charliebob <cxz131330@utdallas.edu>
2016-07-09 18:12:10 -0700

    [372. Super Pow][Accepted]committed by LeetCode Extension

--- 372-Super-Pow/solution.java
+++ 372-Super-Pow/solution.java
@@ -1,28 +1,34 @@
 public class Solution {
-    public int superPow(int a, int[] b) {
-        long mod = (long) a;
-        final long div = 1337L;
-        final List<Integer> list = new ArrayList<>();
-        final Set<Integer> used = new HashSet<>();
-        int key;
-        while (true) {
-            mod %= div;
-            key = (int) mod;
-            // Is the list starting to repeat?
-            if (used.contains(key)) break;
-            list.add(key);
-            used.add(key);
-            mod *= a;
+    int DIV = 1337;
+    
+    List<Integer> findLoop(int a){
+        List<Integer> index = new ArrayList<>();
+        boolean[] set = new boolean[DIV];
+        int rem = a % DIV;
+        while ( ! set[rem] ) {
+            set[rem]=true;
+            index.add(rem);
+            rem = (rem*a) % DIV;
         }
-        // Find the actual mod value from the list
-        // BigInteger was used below to simplify (b mod list_size)
-        // since this problem wasn't about large number division
-        final StringBuilder sb = new StringBuilder();
-        for (int d : b) sb.append(d);
-        key = new BigInteger(sb.toString())
-                .mod(BigInteger.valueOf(list.size())).intValue();
-        if (key == 0) key = list.size();
-        return list.get(key - 1);
-        
+        return index;
+    }
+    
+    int modBy(int[] b, int m){
+        int rem = 0;
+        for (int i=0; i < b.length; i++) {
+            rem = (rem*10+b[i]) % m;
+        }
+        return rem;
+    }
+
+    public int superPow(int a, int[] b) {
+        if (a==0 || a==DIV || b==null || b.length == 0) return 0;
+        if (a==1) return 1;
+        if (a > DIV) return superPow( a % DIV, b);
+        List<Integer> index = findLoop(a);
+        int loopsize = index.size();
+        int rem = modBy(b, loopsize);
+        rem = rem==0? loopsize: rem;
+        return index.get(rem-1);
     }
 }
