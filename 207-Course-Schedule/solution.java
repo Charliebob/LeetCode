9bc93c6ecee8f4162d4fd6e8a03ce8a9e565952d
Charliebob <cxz131330@utdallas.edu>
2016-07-09 18:51:24 -0700

    [207. Course Schedule][Compile Error]committed by LeetCode Extension

--- 207-Course-Schedule/solution.java
+++ 207-Course-Schedule/solution.java
@@ -1,32 +1,30 @@
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList[] graph = new ArrayList[numCourses];
-            for(int i=0;i<numCourses;i++)
-                graph[i] = new ArrayList();
-                
-            boolean[] visited = new boolean[numCourses];
-            for(int i=0; i<prerequisites.length;i++){
-                graph[prerequisites[i][1]].add(prerequisites[i][0]);
+        int[][] matrix = new int[numCourses][numCourses]; // i -> j
+    int[] indegree = new int[numCourses];
+    
+    for (int i=0; i<prerequisites.length; i++) {
+        int ready = prerequisites[i][0];
+        int pre = prerequisites[i][1];
+        if (matrix[pre][ready] == 0)
+            indegree[ready]++; //duplicate case
+        matrix[pre][ready] = 1;
+    }
+    
+    int count = 0;
+    Queue<Integer> queue = new LinkedList();
+    for (int i=0; i<indegree.length; i++) {
+        if (indegree[i] == 0) queue.offer(i);
+    }
+    while (!queue.isEmpty()) {
+        int course = queue.poll();
+        count++;
+        for (int i=0; i<numCourses; i++) {
+            if (matrix[course][i] != 0) {
+                if (--indegree[i] == 0)
+                    queue.offer(i);
             }
-
-            for(int i=0; i<numCourses; i++){
-                if(!dfs(graph,visited,i))
-                    return false;
-            }
-            return true;
         }
-
-        private boolean dfs(ArrayList[] graph, boolean[] visited, int course){
-            if(visited[course])
-                return false;
-            else
-                visited[course] = true;;
-
-            for(int i=0; i<graph[course].size();i++){
-                if(!dfs(graph,visited,(int)graph[course].get(i)))
-                    return false;
-            }
-            visited[course] = false;
-            return true;
-        }
+    }
+    return count == numCourses;
 }
