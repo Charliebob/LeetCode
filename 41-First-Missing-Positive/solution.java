a9e03f8b01d5ad82369220e633fea824e3c04d76
Charliebob <cxz131330@utdallas.edu>
2016-07-09 22:21:46 -0700

    [41. First Missing Positive][Compile Error]committed by LeetCode Extension

--- 41-First-Missing-Positive/solution.java
+++ 41-First-Missing-Positive/solution.java
@@ -1,19 +1,19 @@
 public class Solution {
     public int firstMissingPositive(int[] nums) {
          int i = 0;
-        while(i < A.length){
-            if(A[i] == i+1 || A[i] <= 0 || A[i] > A.length) i++;
-            else if(A[A[i]-1] != A[i]) swap(A, i, A[i]-1);
+        while(i < nums.length){
+            if(nums[i] == i+1 || nums[i] <= 0 || nums[i] > nums.length) i++;
+            else if(nums[nums[i]-1] != nums[i]) swap(nums, i, nums[i]-1);
             else i++;
         }
         i = 0;
-        while(i < A.length && A[i] == i+1) i++;
+        while(i < nums.length && nums[i] == i+1) i++;
         return i+1;
     }
     
-    private void swap(int[] A, int i, int j){
-        int temp = A[i];
-        A[i] = A[j];
-        A[j] = temp;
+    privnumste void swasp(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
