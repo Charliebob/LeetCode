50011f712614b06fa06ee04416e8a1357e7fc7d1
Charliebob <cxz131330@utdallas.edu>
2016-07-09 18:43:58 -0700

    [46. Permutations][Accepted]committed by LeetCode Extension

--- 46-Permutations/solution.java
+++ 46-Permutations/solution.java
@@ -1,29 +1,30 @@
 public class Solution {
+    List<List<Integer>> list;
+
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-    perm(result,nums,0,nums.length-1);
-    return result;
-}
-public static void perm(List<List<Integer>> result, int[] nums, int start, int end){
-    if(start==end){
-        Integer[] ele = new Integer[nums.length];
-        for(int i=0; i<nums.length; i++){
-            ele[i] = nums[i];
-        }
-        result.add(Arrays.asList(ele));
+        
+        list = new ArrayList<>();
+        ArrayList<Integer> perm = new ArrayList<Integer>();
+        backTrack(perm,0,nums);
+        return list;
     }
-    else{
-        for(int i=start; i<=end; i++){
-            int temp = nums[start];
-            nums[start] = nums[i];
-            nums[i] = temp;
-
-            perm(result, nums,start+1,end);
-
-            temp = nums[start];
-            nums[start] = nums[i];
-            nums[i] = temp;
+    
+    void backTrack (ArrayList<Integer> perm,int i,int[] nums){
+        
+        //Permutation completes
+        if(i==nums.length){
+            list.add(new ArrayList(perm));
+            return;
         }
-    }
+        
+        ArrayList<Integer> newPerm = new ArrayList<Integer>(perm);
+        
+       //Insert elements in the array by increasing index
+        for(int j=0;j<=i;j++){
+            newPerm.add(j,nums[i]);
+            backTrack(newPerm,i+1,nums);
+            newPerm.remove(j);
+        }
+        
     }
 }
