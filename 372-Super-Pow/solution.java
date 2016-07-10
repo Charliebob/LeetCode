fd51f08e6ae45f868d9a1ac7889c4d2e1d813bda
Charliebob <cxz131330@utdallas.edu>
2016-07-09 18:09:43 -0700

    [372. Super Pow][Compile Error]committed by LeetCode Extension

---
+++ 372-Super-Pow/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int superPow(int a, int[] b) {
+        int result = 1;
+        while (b != 0) {
+            if (b % 2 != 0)
+                result = result * a % M;
+            a = a * a % M;
+            b /= 2;
+        }
+        return result;
+    }
+
+    public int superPow(int a, int[] b) {
+        a %= M;
+        int result = 1;
+        for (int i = b.length - 1; i >= 0; i--) {
+            result = result * normalPow(a, b[i]) % M;
+            a = normalPow(a, 10);
+        }
+        return result;
+    }
+}
