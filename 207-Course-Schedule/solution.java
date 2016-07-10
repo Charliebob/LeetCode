c01a53f3c1aff3dd8684fc41db07a35da5499403
Charliebob <cxz131330@utdallas.edu>
2016-07-09 18:52:36 -0700

    [207. Course Schedule][Accepted]committed by LeetCode Extension

--- 207-Course-Schedule/solution.java
+++ 207-Course-Schedule/solution.java
@@ -1,32 +1,30 @@
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[][] matrix = new int[numCourses][numCourses]; // i -> j
-        int[] indegree = new int[numCourses];
-        
-        for (int i=0; i<prerequisites.length; i++) {
-            int ready = prerequisites[i][0];
-            int pre = prerequisites[i][1];
-            if (matrix[pre][ready] == 0)
-                indegree[ready]++; //duplicate case
-            matrix[pre][ready] = 1;
-        }
-        
-        int count = 0;
-        Queue<Integer> queue = new LinkedList();
-        for (int i=0; i<indegree.length; i++) {
-            if (indegree[i] == 0) queue.offer(i);
-        }
-        while (!queue.isEmpty()) {
-            int course = queue.poll();
-            count++;
-            for (int i=0; i<numCourses; i++) {
-                if (matrix[course][i] != 0) {
-                    if (--indegree[i] == 0)
-                        queue.offer(i);
-                }
-            }
-        }
-        return count == numCourses;
-        
+        if (numCourses <= 0)
+    		return false;
+    	Queue<Integer> queue = new LinkedList<>();
+    	int[] inDegree = new int[numCourses];
+    	for (int i = 0; i < prerequisites.length; i++) {
+    		inDegree[prerequisites[i][1]]++;
+    	}
+    	for (int i = 0; i < inDegree.length; i++) {
+    		if (inDegree[i] == 0)
+    			queue.offer(i);
+    	}
+    	while (!queue.isEmpty()) {
+    		int x = queue.poll();
+    		for (int i = 0; i < prerequisites.length; i++) {
+    			if (x == prerequisites[i][0]) {
+    				inDegree[prerequisites[i][1]]--;
+    				if (inDegree[prerequisites[i][1]] == 0)
+    					queue.offer(prerequisites[i][1]);
+    			}
+    		}
+    	}
+    	for (int i = 0; i < inDegree.length; i++) {
+    		if (inDegree[i] != 0)
+    			return false;
+    	}
+    	return true;
     }
 }
