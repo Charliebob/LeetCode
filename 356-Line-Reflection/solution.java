d25826dfac9ae8795f60d22d7a4c336ad79c6470
Charliebob <cxz131330@utdallas.edu>
2016-07-09 21:50:01 -0700

    [356. Line Reflection][Accepted]committed by LeetCode Extension

--- 356-Line-Reflection/solution.java
+++ 356-Line-Reflection/solution.java
@@ -6,15 +6,16 @@ public class Solution {
         for(int[] p:points){
             max = Math.max(max,p[0]);
             min = Math.min(min,p[0]);
-            String str = p[0] + "a" + p[1];
+            String str = p[0] + "a" + p[1]; //p[0]==x  p[1]==y
             set.add(str);
         }
         int sum = max+min;
         for(int[] p:points){
             //int[] arr = {sum-p[0],p[1]};
-            String str = (sum-p[0]) + "a" + p[1];
+            String str = (sum-p[0]) + "a" + p[1]; //find symmetric
             if( !set.contains(str))
                 return false;
         }
+        return true;
     }
 }
