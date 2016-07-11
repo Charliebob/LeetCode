9526074683e5855ba6e2d4fbc95457cd53a5beb7
Charliebob <cxz131330@utdallas.edu>
2016-07-10 21:19:08 -0700

    [269. Alien Dictionary][Accepted]committed by LeetCode Extension

--- 269-Alien-Dictionary/solution.java
+++ 269-Alien-Dictionary/solution.java
@@ -1,7 +1,7 @@
 public class Solution {
     public String alienOrder(String[] words) {
         Map<Character, Set<Character>> map = new HashMap<Character, Set<Character>>();
-        Map<Character, Integer> degeree = new HashMap<Character, Integer>();
+        Map<Character, Integer> degree = new HashMap<Character, Integer>();
         String result = "";
         if(words==null || words.length==0) return result;
         for(String s: words){
