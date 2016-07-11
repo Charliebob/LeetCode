fcab2543e716bb32d5f1373eb82381920fbc482f
Charliebob <cxz131330@utdallas.edu>
2016-07-10 21:23:30 -0700

    [269. Alien Dictionary][Accepted]committed by LeetCode Extension

--- 269-Alien-Dictionary/solution.java
+++ 269-Alien-Dictionary/solution.java
@@ -1,48 +1,52 @@
 public class Solution {
+    private final int N = 26;
     public String alienOrder(String[] words) {
-        Map<Character, Set<Character>> map = new HashMap<Character, Set<Character>>();
-        Map<Character, Integer> degree = new HashMap<Character, Integer>();
-        String result = "";
-        if(words==null || words.length==0) return result;
-        for(String s: words){
-            for(char c: s.toCharArray()){
-                degree.put(c, 0);
+        List<Set<Integer>> adj = new ArrayList<>(N);
+        int[] degree = new int[N];
+        buildGraph(words, adj, degree);
+        
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < N; i++) {
+            if(degree[i] == 0) q.add(i);
+        }
+        // peeling 0 indegree nodes
+        while(!q.isEmpty()) {
+            int i = q.poll();
+            sb.append((char) ('a' + i));
+            for(int j : adj.get(i)) {
+                if(--degree[j] == 0) {
+                    q.add(j);
+                }
             }
         }
-        for(int i=0; i<words.length-1; i++){
-            String cur = words[i];
-            String next = words[i+1];
-            int length = Math.min(cur.length(), next.length());
-            for(int j=0; j<length; j++){
-                char c1 = cur.charAt(j);
-                char c2 = next.charAt(j);
-                if(c1!=c2){
-                    Set<Character> set = new HashSet<Character>();
-                    if(map.containsKey(c1)) set = map.get(c1);
-                    if(!set.contains(c2)){
-                        set.add(c2);
-                        map.put(c1,set);
-                        degree.put(c2, degree.get(c2)+1);
+        for(int d : degree) if(d > 0) return "";  // invalid
+        return sb.toString();
+    }
+    
+    public void buildGraph(String[] words, List<Set<Integer>> adj, int[] degree) {
+        for(int i = 0; i < N; i++) adj.add(new HashSet<Integer>());
+        Arrays.fill(degree, -1);
+    
+        for(int i = 0; i < words.length; i++) {
+            for(char c : words[i].toCharArray()) { 
+                if(degree[c - 'a'] < 0) degree[c - 'a'] = 0;
+            }
+            if(i > 0) {
+                String w1 = words[i - 1], w2 = words[i];
+                int len = Math.min(w1.length(), w2.length());
+                for(int j = 0; j < len; j++) {
+                    if(w1.charAt(j) != w2.charAt(j)) {
+                        int c1 = w1.charAt(j) - 'a', c2 = w2.charAt(j) - 'a';
+                        // c1 -> c2, c1 is lexically earlier to c2.
+                        if(!adj.get(c1).contains(c2)) {
+                            adj.get(c1).add(c2);
+                            degree[c2]++;
+                            break;
+                        }
                     }
-                    break;
                 }
             }
         }
-        Queue<Character> queue = new LinkedList<Character>();
-        for(char c: degree.keySet()){
-            if(degree.get(c)==0) queue.add(c);
-        }
-        while(!queue.isEmpty()){
-            char c = queue.remove();
-            result+=c;
-            if(map.containsKey(c)){
-                for(char c2:map.get(c)){
-                    degree.put(c2, degree.get(c2)-1);
-                    if(degree.get(c2)==0) queue.add(c2);
-                }
-            }
-        }
-        if(result.length()!=degree.size()) return "";
-        return result;
     }
 }
