d275b92446439e0e1d662cc321eb9161d0ae2f8a
Charliebob <cxz131330@utdallas.edu>
2016-07-11 13:10:36 -0700

    [281. Zigzag Iterator][Accepted]committed by LeetCode Extension

--- 281-Zigzag-Iterator/solution.java
+++ 281-Zigzag-Iterator/solution.java
@@ -1,17 +1,20 @@
 public class ZigzagIterator {
-    private Iterator<Integer> i, j, temp;
+    LinkedList<Iterator> list;
     public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
-        i = v2.iterator();
-        j = v1.iterator();
+        list = new LinkedList<Iterator>();
+        if(!v1.isEmpty()) list.add(v1.iterator());
+        if(!v2.isEmpty()) list.add(v2.iterator());
     }
 
     public int next() {
-        if(j.hasNext()){temp =j; j = i; i=temp;}
-        return i.next();
+        Iterator poll = list.remove();
+        int result = (Integer)poll.next();
+        if(poll.hasNext()) list.add(poll);
+        return result;
     }
 
     public boolean hasNext() {
-        return i.hasNext() || j.hasNext();
+        return !list.isEmpty();
     }
 }
 
