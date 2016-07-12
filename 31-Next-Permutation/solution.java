8d611e40c6601e37d64b0c4454851dd7c02abda9
Charliebob <cxz131330@utdallas.edu>
2016-07-12 16:33:45 -0700

    [31. Next Permutation][Accepted]committed by LeetCode Extension

--- 31-Next-Permutation/solution.java
+++ 31-Next-Permutation/solution.java
@@ -1,36 +1,23 @@
 public class Solution {
     public void nextPermutation(int[] nums) {
-        if(nums.length<2) return;
-        int index = nums.length-1;
-        while(index>0){
-            if(nums[index]>nums[index-1]){
-                break;
-            }
-            index--;
+        if(nums == null || nums.length <= 1) return;
+        int i = nums.length - 2;
+        while(i >= 0 && nums[i] >= nums[i + 1]) i--; // Find 1st id i that breaks descending order
+        if(i >= 0) {                           // If not entirely descending
+            int j = nums.length - 1;              // Start from the end
+            while(nums[j] <= nums[i]) j--;           // Find rightmost first larger id j
+            swap(nums, i, j);                     // Switch i and j
         }
-        if(index==0){
-            Arrays.sort(nums);
-        }else{
-            int val = nums[index-1];
-            int j = nums.length-1;
-            while(j>=index){
-                if(nums[j]>val) break;
-                j--;
-            }
-            swap(nums,j,index-1);
-            reverseSort(nums,index,nums.length-1);
-            return;
-        }
+        reverse(nums, i + 1, nums.length - 1);       // Reverse the descending sequence
     }
-    public void reverseSort(int[] nums, int start, int end){   
-        if(start>end)
-            return;
-        for(int i=start;i<=(end+start)/2;i++)
-            swap(nums,i,start+end-i);
+    
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
-    public void swap(int[] nums, int i, int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+    
+    public void reverse(int[] nums, int i, int j) {
+        while(i < j) swap(nums, i++, j--);
     }
 }
