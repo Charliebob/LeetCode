1a52c5f755216c8d65d6bd47508ef598d9134f3f
Charliebob <cxz131330@utdallas.edu>
2016-07-13 14:49:05 -0700

    [294. Flip Game II][Accepted]committed by LeetCode Extension

--- 294-Flip-Game-II/solution.java
+++ 294-Flip-Game-II/solution.java
@@ -1,22 +1,14 @@
 public class Solution {
     public boolean canWin(String s) {
-        if(s == null || s.length() < 2) return false;
-        Map<String, Boolean> winMap = new HashMap<String, Boolean>();
-        return canWin(s, winMap);
-    }
-
-    public boolean canWin(String s, Map<String, Boolean> winMap){
-        if(winMap.containsKey(s)) return winMap.get(s);
-        for(int i = 0; i < s.length() - 1; i++) {
-            if(s.charAt(i) == '+' && s.charAt(i + 1) == '+') {
-                String sOpponent = s.substring(0, i) + "--" + s.substring(i + 2);
-                if(!canWin(sOpponent, winMap)) {
-                    winMap.put(s, true);
+        if(s==null || s.length()<2) return false;
+        for(int i=0; i<s.length()-1; i++){
+            if(s.startsWith("++", i)){
+                String t = s.substring(0,i)+"--"+s.substring(i+2);
+                if(!canWin(t)){
                     return true;
                 }
             }
         }
-        winMap.put(s, false);
         return false;
     }
 }
