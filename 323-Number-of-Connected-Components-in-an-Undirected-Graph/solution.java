b6e5a581e2cc36ae06e6666c41ca0ad2c5267d91
Charliebob <cxz131330@utdallas.edu>
2016-07-14 10:55:20 -0700

    [323. Number of Connected Components in an Undirected Graph][Accepted]committed by LeetCode Extension

--- 323-Number-of-Connected-Components-in-an-Undirected-Graph/solution.java
+++ 323-Number-of-Connected-Components-in-an-Undirected-Graph/solution.java
@@ -1,27 +1,25 @@
 public class Solution {
     public int countComponents(int n, int[][] edges) {
         int[] id = new int[n];
-        // 初始化
-        for (int i = 0; i < n; i++) {
-            id[i] = i;
-        }
-        // union
-        for (int[] edge : edges) {              
+        //initialization
+        for(int i=0; i<n; i++) id[i] = i;
+        //union
+        for(int[] edge: edges){
             int i = root(id, edge[0]);
             int j = root(id, edge[1]);
             id[i] = j;
         }
-        // 统计根节点个数
+        //find number of root
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (id[i] == i)
-                count++;
+        for(int i=0; i<n; i++){
+            if(id[i]==i) count++;
         }
         return count;
+        
     }
-    // 找根节点
-    public int root(int[] id, int i) {
-        while (i != id[i]) {
+    //find root id
+    public int root(int[] id, int i){
+        while(i!=id[i]){
             id[i] = id[id[i]];
             i = id[i];
         }
