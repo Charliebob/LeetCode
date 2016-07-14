cfbb746ed642043b1a124bc6488f743216f7176e
Charliebob <cxz131330@utdallas.edu>
2016-07-14 13:58:01 -0700

    [169. Majority Element][Compile Error]committed by LeetCode Extension

--- 169-Majority-Element/solution.java
+++ 169-Majority-Element/solution.java
@@ -3,14 +3,13 @@ public class Solution {
         HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
         //key: nums[i], value: times;
         for(int i=0; i<nums.length; i++){
-            int times;
             if(hm.containsKey(nums[i])){
-                times = hm.get(nums[i])+1;
+                hm.put(nums[i], hm.get(nums[i])+1);
             }else{
-                times = 1;
+                hm.put(nums[i], 1);
             }if(times>nums.length/2){
                 return nums[i];
-            }hm.put(nums[i], times);
+            }
         }
         return 0;
     }
