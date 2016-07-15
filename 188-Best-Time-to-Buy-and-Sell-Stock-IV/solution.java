779c784e83306e16ce5af5864f1f7960178e33b1
Charliebob <cxz131330@utdallas.edu>
2016-07-14 17:03:31 -0700

    [188. Best Time to Buy and Sell Stock IV][Time Limit Exceeded]committed by LeetCode Extension

--- 188-Best-Time-to-Buy-and-Sell-Stock-IV/solution.java
+++ 188-Best-Time-to-Buy-and-Sell-Stock-IV/solution.java
@@ -1,25 +1,28 @@
 public class Solution {
     public int maxProfit(int k, int[] prices) {
-        int len = prices.length;
-        if (k >= len / 2) return quickSolve(prices);
-        
-        int[][] t = new int[k + 1][len];
+        int n = prices.length;
+    	if (n <= 1)
+    		return 0;
+    	
+    	//if k >= n/2, then you can make maximum number of transactions.
+    	if (k >=  n/2) {
+    		int maxPro = 0;
+    		for (int i = 1; i < n; i++) {
+    			if (prices[i] > prices[i-1])
+    				maxPro += prices[i] - prices[i-1];
+    		}
+    		return maxPro;
+    	}
+    	
+        int[][] dp = new int[k+1][n];
         for (int i = 1; i <= k; i++) {
-            int tmpMax =  -prices[0];
-            for (int j = 1; j < len; j++) {
-                t[i][j] = Math.max(t[i][j - 1], prices[j] + tmpMax);
-                tmpMax =  Math.max(tmpMax, t[i - 1][j - 1] - prices[j]);
-            }
+        	int localMax = dp[i-1][0] - prices[0];
+        	for (int j = 1; j < n; j++) {
+        		dp[i][j] = Math.max(dp[i][j-1],  prices[j] + localMax);
+        		localMax = Math.max(localMax, dp[i-1][j] - prices[j]);
+        	}
         }
-        return t[k][len - 1];
-    }
-    
-
-    private int quickSolve(int[] prices) {
-        int len = prices.length, profit = 0;
-        for (int i = 1; i < len; i++)
-            // as long as there is a price gap, we gain a profit.
-            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
-        return profit;
+        return dp[k][n-1];
+        
     }
 }
