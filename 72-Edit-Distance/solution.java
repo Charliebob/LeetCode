8157b83d87212956d325ab16f3598e4853e2c1f3
Charliebob <cxz131330@utdallas.edu>
2016-07-15 15:46:26 -0700

    [72. Edit Distance][Accepted]committed by LeetCode Extension

--- 72-Edit-Distance/solution.java
+++ 72-Edit-Distance/solution.java
@@ -1,27 +1,21 @@
 public class Solution {
     public int minDistance(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
-        
-        int[][] cost = new int[m + 1][n + 1];
-        for(int i = 0; i <= m; i++)
-            cost[i][0] = i;
-        for(int i = 1; i <= n; i++)
-            cost[0][i] = i;
-        
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(word1.charAt(i) == word2.charAt(j))
-                    cost[i + 1][j + 1] = cost[i][j];
-                else {
-                    int a = cost[i][j];
-                    int b = cost[i][j + 1];
-                    int c = cost[i + 1][j];
-                    cost[i + 1][j + 1] = a < b ? (a < c ? a : c) : (b < c ? b : c);
-                    cost[i + 1][j + 1]++;
-                }
-            }
-        }
-        return cost[m][n];
+        int[][] dp = new int[word1.length()+1][word2.length()+1];  
+        for(int i=0; i <= word1.length(); i++)  
+            dp[i][0]=i;  
+        for(int j=0; j <= word2.length(); j++)  
+            dp[0][j]=j;  
+
+        for(int i=1; i <= word1.length(); i++){  
+            for(int j=1; j <= word2.length(); j++){  
+                if(word1.charAt(i-1) == word2.charAt(j-1)){  
+                    dp[i][j]=dp[i-1][j-1];  
+                }  
+                else{  
+                    dp[i][j]=1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));  
+                }  
+            }  
+        }  
+        return dp[word1.length()][word2.length()]; 
     }
 }
