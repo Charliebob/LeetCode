efb31538d7747d2ce50637eb9b8c57e024953479
Charliebob <cxz131330@utdallas.edu>
2016-07-16 12:04:33 -0700

    [97. Interleaving String][Compile Error]committed by LeetCode Extension

--- 97-Interleaving-String/solution.java
+++ 97-Interleaving-String/solution.java
@@ -13,8 +13,12 @@ public class Solution {
         }
         for (int i = 1; i < matrix.length; i++){
             for (int j = 1; j < matrix[0].length; j++){
-                matrix[i][j] = (matrix[i-1][j]&&(s2.charAt(i-1)==s3.charAt(i+j-1)))
-                        || (matrix[i][j-1]&&(s1.charAt(j-1)==s3.charAt(i+j-1)));
+                if(s2.charAt(i-1)==s3.charAt(i+j-1))){
+                    matrix[i][j] = matrix[i-1][j];
+                }
+                if(s1.charAt(j-1)==s3.charAt(i+j-1)){
+                    matrix[i][j] = matrix[i][j-1];
+                }
             }
         }
         return matrix[s2.length()][s1.length()];
