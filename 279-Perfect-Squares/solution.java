3b084704c87e17a47bfd36518542891f7174d560
Charliebob <cxz131330@utdallas.edu>
2016-07-16 10:56:46 -0700

    [279. Perfect Squares][Accepted]committed by LeetCode Extension

--- 279-Perfect-Squares/solution.java
+++ 279-Perfect-Squares/solution.java
@@ -1,11 +1,18 @@
 public class Solution {
     public int numSquares(int n) {
         int[] dp = new int[n+1];
+        // 将所有非平方数的结果置最大，保证之后比较的时候不被选中
         Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0]= 0;
-        for(int i=0; i<=n; i++){
-            for(int j=1; i+j*j<=n; j++){
-                dp[i+j*j] = Math.min(dp[i+j*j],dp[i]+1);
+        // 将所有平方数的结果置1
+        for(int i = 0; i * i <= n; i++){
+            dp[i * i] = 1;
+        }
+        // 从小到大找任意数a
+        for(int a = 0; a <= n; a++){
+            // 从小到大找平方数bｘb
+            for(int b = 0; a + b * b <= n; b++){
+                // 因为a+b*b可能本身就是平方数，所以我们要取两个中较小的
+                dp[a + b * b] = Math.min(dp[a] + 1, dp[a + b * b]);
             }
         }
         return dp[n];
