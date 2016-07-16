8cb134834ce97d1146fcd45286ff96d9ef4676ff
Charliebob <cxz131330@utdallas.edu>
2016-07-16 11:45:58 -0700

    [97. Interleaving String][Runtime Error]committed by LeetCode Extension

--- 97-Interleaving-String/solution.java
+++ 97-Interleaving-String/solution.java
@@ -19,12 +19,12 @@ public class Solution {
                 dp[i][0] = false;
             }
         }
-        for(int i=0; i<s1.length(); i++){
-            for(int j=0; j<s2.length(); j++){
-                if(s3.charAt(i+j)==s1.charAt(i)){
+        for(int i=1; i<s1.length(); i++){
+            for(int j=1; j<s2.length(); j++){
+                if(s3.charAt(i+j-1)==s1.charAt(i)){
                     dp[i][j] = dp[i-1][j];
                 }
-                if(s3.charAt(i+j)==s2.charAt(j)){
+                if(s3.charAt(i+j-1)==s2.charAt(j)){
                     dp[i][j] = dp[i][j-1];
                 }else{
                     dp[i][j] = false;
