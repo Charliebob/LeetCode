3068b103f05fe4273f2aa51753ed107253dd2ba4
Charliebob <cxz131330@utdallas.edu>
2016-07-15 21:48:05 -0700

    [63. Unique Paths II][Runtime Error]committed by LeetCode Extension

--- 63-Unique-Paths-II/solution.java
+++ 63-Unique-Paths-II/solution.java
@@ -21,7 +21,7 @@ public class Solution {
         }
         for(int i=1; i<row; i++){
             for(int j=1; j<col; j++){
-                if(obstacle[i][j]==1){
+                if(obstacleGrid[i][j]==1){
                     for(int k=i; k<row; k++) dp[k][j] = 0;
                     for(int l=j; j<col; l++) dp[i][l] = 0;
                 }else{
