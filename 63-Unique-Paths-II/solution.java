5a1085bdbd8fdb2a0d4aeb5f583fa5600a00ecae
Charliebob <cxz131330@utdallas.edu>
2016-07-15 21:50:17 -0700

    [63. Unique Paths II][Accepted]committed by LeetCode Extension

--- 63-Unique-Paths-II/solution.java
+++ 63-Unique-Paths-II/solution.java
@@ -1,34 +1,16 @@
 public class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int row = obstacleGrid.length;
-        int col = obstacleGrid[0].length;
-        int[][] dp = new int[row][col];
-        for(int i=0; i<row; i++){
-            if(obstacleGrid[i][0]==1){
-                for(int j=i; j<row; j++) dp[j][0] = 0;
-                break;
-            }else{
-                dp[i][0] = 1;
+        int width = obstacleGrid[0].length;
+        int[] dp = new int[width];
+        dp[0] = 1;
+        for (int[] row : obstacleGrid) {
+            for (int j = 0; j < width; j++) {
+                if (row[j] == 1)
+                    dp[j] = 0;
+                else if (j > 0)
+                    dp[j] += dp[j - 1];
             }
         }
-        for(int i=0; i<col; i++){
-            if(obstacleGrid[0][i]==1){
-                for(int j=i; j<col; j++) dp[0][j] = 0;
-                break;
-            }else{
-                dp[0][i] = 1;
-            }
-        }
-        for(int i=1; i<row; i++){
-            for(int j=1; j<col; j++){
-                if(obstacleGrid[i][j]==1){
-                    for(int k=i; k<row; k++) dp[k][j] = 0;
-                    for(int l=j; l<col; l++) dp[i][l] = 0;
-                }else{
-                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
-                }
-            }
-        }
-        return dp[row-1][col-1];
+        return dp[width - 1];
     }
 }
