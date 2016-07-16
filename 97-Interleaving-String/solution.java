a1a29f9be9a0801acb0d9a73bc3f96db6bb6ae58
Charliebob <cxz131330@utdallas.edu>
2016-07-16 12:02:35 -0700

    [97. Interleaving String][Accepted]committed by LeetCode Extension

--- 97-Interleaving-String/solution.java
+++ 97-Interleaving-String/solution.java
@@ -7,7 +7,8 @@ public class Solution {
             matrix[0][i] = matrix[0][i-1]&&(s1.charAt(i-1)==s3.charAt(i-1));
         }
         for (int i = 1; i < matrix.length; i++){
-            matrix[i][0] = matrix[i-1][0]&&(s2.charAt(i-1)==s3.charAt(i-1));
+            if((s2.charAt(i-1)==s3.charAt(i-1)))
+                matrix[i][0] = matrix[i-1][0];
         }
         for (int i = 1; i < matrix.length; i++){
             for (int j = 1; j < matrix[0].length; j++){
