8e6baa4a3ac01dea4cb5a79bf76d0e8e4c74bdff
Charliebob <cxz131330@utdallas.edu>
2016-07-16 20:18:45 -0700

    [300. Longest Increasing Subsequence][Accepted]committed by LeetCode Extension

--- 300-Longest-Increasing-Subsequence/solution.java
+++ 300-Longest-Increasing-Subsequence/solution.java
@@ -1,15 +1,39 @@
 public class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        int len = 0;
+        // Base case
+		if(nums.length <= 1) 
+			return nums.length;
+		// This will be our array to track longest sequence length
+		int T[] = new int[nums.length];
 
-        for(int x : nums) {
-            int i = Arrays.binarySearch(dp, 0, len, x);
-            if(i < 0) i = -(i + 1);
-            dp[i] = x;
-            if(i == len) len++;
-        }
+		// Fill each position with value 1 in the array
+		for(int i=0; i < nums.length; i++)
+			T[i] = 1;
+        int longest = 0;
+		// Mark one pointer at i. For each i, start from j=0.
+		for(int i=1; i < nums.length; i++)
+		{
+			for(int j=0; j < i; j++)
+			{
+				// It means next number contributes to increasing sequence.
+				if(nums[j] < nums[i])
+				{
+					// But increase the value only if it results in a larger value of the sequence than T[i]
+					// It is possible that T[i] already has larger value from some previous j'th iteration
+					if(T[j] + 1 > T[i])
+					{
+						T[i] = T[j] + 1;
+					}
+				}
+				longest = Math.max(longest, T[i]);
+			}
+		}
 
-        return len;
+		// Find the maximum length from the array that we just generated 
+// 		int longest = 0;
+// 		for(int i=0; i < T.length; i++)
+// 			longest = Math.max(longest, T[i]);
+
+		return longest;
     }
 }
