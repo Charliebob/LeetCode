705a5bdaf47d7b7751849cb8ea2ede38098a60e1
Charliebob <cxz131330@utdallas.edu>
2016-07-18 15:55:03 -0700

    [79. Word Search][Accepted]committed by LeetCode Extension

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -1,30 +1,37 @@
 public class Solution {
+    static boolean[][] visited;
     public boolean exist(char[][] board, String word) {
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[i].length;j++){
-                if(helper(board,i,j,word,0)){
+        visited = new boolean[board.length][board[0].length];
+        
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if((word.charAt(0) == board[i][j]) && search(board, word, i, j, 0)){
                     return true;
                 }
             }
         }
+        
         return false;
     }
-
-    private boolean helper(char[][] board, int i, int j, String word, int step){
-        if(step==word.length()){
+    
+    private boolean search(char[][]board, String word, int i, int j, int index){
+        if(index == word.length()){
             return true;
         }
-        if(i<0||i>=board.length||j<0||j>=board[i].length){
+        
+        if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word.charAt(index) || visited[i][j]){
             return false;
         }
-        if((board[i][j]-word.charAt(step))!=0){
-            return false;
+        
+        visited[i][j] = true;
+        if(search(board, word, i-1, j, index+1) || 
+           search(board, word, i+1, j, index+1) ||
+           search(board, word, i, j-1, index+1) || 
+           search(board, word, i, j+1, index+1)){
+            return true;
         }
-        char record=board[i][j];
-        board[i][j]='1';
-        boolean res=helper(board,i-1,j,word,step+1)||helper(board,i,j-1,word,step+1)||helper(board,i,j+1,word,step+1)||helper(board,i+1,j,word,step+1);
-        board[i][j]=record;
-        return res;
-
+        
+        visited[i][j] = false;
+        return false;
     }
 }
