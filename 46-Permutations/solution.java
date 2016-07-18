dc7ff93da28d69c941b8e1f9e44e763e98e029a1
Charliebob <cxz131330@utdallas.edu>
2016-07-18 16:36:32 -0700

    [46. Permutations][Accepted]committed by LeetCode Extension

--- 46-Permutations/solution.java
+++ 46-Permutations/solution.java
@@ -1,20 +1,19 @@
 public class Solution {
     
-
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> list = new ArrayList<List<Integer>>();
-        ArrayList<Integer> perm = new ArrayList<Integer>();
+        List<Integer> perm = new ArrayList<Integer>();
         backTrack(list, perm, 0, nums);
         return list;
     }
     
-    private void backTrack (List<List<Integer>> list, ArrayList<Integer> perm, int i, int[] nums){
+    private void backTrack (List<List<Integer>> list, List<Integer> perm, int i, int[] nums){
         //Permutation completes
         if(i==nums.length){
             list.add(new ArrayList(perm));
             return;
         }
-        ArrayList<Integer> newPerm = new ArrayList<Integer>(perm);
+        List<Integer> newPerm = new ArrayList<Integer>(perm);
        //Insert elements in the array by increasing index
         for(int j=0;j<=i;j++){
             newPerm.add(j,nums[i]);
