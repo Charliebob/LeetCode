26110ada5453613d78bdcaf404f08769aacb8980
Charliebob <cxz131330@utdallas.edu>
2016-07-18 15:55:59 -0700

    [79. Word Search][Accepted]committed by LeetCode Extension

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -1,5 +1,5 @@
 public class Solution {
-    static boolean[][] visited;
+    private boolean[][] visited;
     public boolean exist(char[][] board, String word) {
         visited = new boolean[board.length][board[0].length];
         
@@ -10,19 +10,14 @@ public class Solution {
                 }
             }
         }
-        
         return false;
     }
     
     private boolean search(char[][]board, String word, int i, int j, int index){
-        if(index == word.length()){
-            return true;
-        }
-        
+        if(index == word.length()) return true;
         if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word.charAt(index) || visited[i][j]){
             return false;
         }
-        
         visited[i][j] = true;
         if(search(board, word, i-1, j, index+1) || 
            search(board, word, i+1, j, index+1) ||
@@ -30,7 +25,6 @@ public class Solution {
            search(board, word, i, j+1, index+1)){
             return true;
         }
-        
         visited[i][j] = false;
         return false;
     }
