a48bb17fa2c139f65cdbde77964a1e8344d4ed3a
Charliebob <cxz131330@utdallas.edu>
2016-07-18 09:59:48 -0700

    [354. Russian Doll Envelopes][Accepted]committed by LeetCode Extension

--- 354-Russian-Doll-Envelopes/solution.java
+++ 354-Russian-Doll-Envelopes/solution.java
@@ -1,24 +1,26 @@
 public class Solution {
     public int maxEnvelopes(int[][] envelopes) {
-        if(envelopes==null || envelopes.length==0 || envelopes[0].length==0) return 0;
+        if(envelopes == null || envelopes.length == 0 
+           || envelopes[0] == null || envelopes[0].length != 2)
+            return 0;
         Arrays.sort(envelopes, new Comparator<int[]>(){
-            @Override
-            public int compare(int[] e1, int[] e2){
-                return Integer.compare(e1[0], e2[0]); //compare by width
-            }
+            public int compare(int[] arr1, int[] arr2){
+                if(arr1[0] == arr2[0])
+                    return arr2[1] - arr1[1];
+                else
+                    return arr1[0] - arr2[0];
+           } 
         });
-        int n  = envelopes.length;
-        int[] dp = new int[n];
-        int ret = 0;
-        for (int i = 0; i < n; i++){
-            dp[i] = 1;
-            for (int j = 0; j < i; j++){
-                if (envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1]){ //if width and length are greater
-                    dp[i] = Math.max(dp[i], 1 + dp[j]);                                      //than previous one
-                }
-            }
-            ret = Math.max(ret, dp[i]);
+        int dp[] = new int[envelopes.length];
+        int len = 0;
+        for(int[] envelope : envelopes){
+            int index = Arrays.binarySearch(dp, 0, len, envelope[1]);
+            if(index < 0)
+                index = -(index + 1);
+            dp[index] = envelope[1];
+            if(index == len)
+                len++;
         }
-        return ret;
+        return len;
     }
 }
