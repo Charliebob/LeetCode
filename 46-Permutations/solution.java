2ca86f9bd105ec8a7eea2ca88f0a61093c28fd3b
Charliebob <cxz131330@utdallas.edu>
2016-07-18 16:25:24 -0700

    [46. Permutations][Compile Error]committed by LeetCode Extension

--- 46-Permutations/solution.java
+++ 46-Permutations/solution.java
@@ -1,24 +1,20 @@
 public class Solution {
-    List<List<Integer>> list;
+    
 
     public List<List<Integer>> permute(int[] nums) {
-        
-        list = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
         ArrayList<Integer> perm = new ArrayList<Integer>();
-        backTrack(perm,0,nums);
+        backTrack(list, perm, 0, nums);
         return list;
     }
     
-    void backTrack (ArrayList<Integer> perm,int i,int[] nums){
-        
+    void backTrack (List<List<Integer>> list, ArrayList<Integer> perm, int i, int[] nums){
         //Permutation completes
         if(i==nums.length){
             list.add(new ArrayList(perm));
             return;
         }
-        
         ArrayList<Integer> newPerm = new ArrayList<Integer>(perm);
-        
        //Insert elements in the array by increasing index
         for(int j=0;j<=i;j++){
             newPerm.add(j,nums[i]);
