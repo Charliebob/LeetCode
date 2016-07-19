9f8de9588c9f85a450c2ef2acc67fc35b22cf2e5
Charliebob <cxz131330@utdallas.edu>
2016-07-18 22:28:26 -0700

    [39. Combination Sum][Accepted]committed by LeetCode Extension

--- 39-Combination-Sum/solution.java
+++ 39-Combination-Sum/solution.java
@@ -1,17 +1,20 @@
 public class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        combinationSum(result,new ArrayList<Integer>(),candidates,target,0);
+        getResult(result, new ArrayList<Integer>(), candidates, target, 0);
         return result;
     }
-    public void combinationSum(List<List<Integer>> result, List<Integer> cur, int[] candidates, int target,int start) {
-        if (target > 0) {
-            for (int i = start;i < candidates.length;i++) { // not using the condition "target >= candidates[i]"
+    private void getResult(List<List<Integer>> result, List<Integer>cur, int candidates[], int target, int start){
+        if(target==0){
+            result.add(new ArrayList<Integer>(cur));
+        }
+        else{
+            for(int i=start; i<candidates.length && target>=candidates[i];i++){
                 cur.add(candidates[i]);
-                combinationSum(result, cur, candidates, target-candidates[i],i);
-                cur.remove(cur.size() - 1);
+                getResult(result, cur, candidates, target-candidates[i], i);
+                cur.remove(cur.size()-1);
             }
         }
-        if (target == 0) result.add(new ArrayList<Integer>(cur));
     }
 }
