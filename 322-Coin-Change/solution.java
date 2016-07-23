d651f8947646c25e0f75b461fa54268742adab1a
Charliebob <cxz131330@utdallas.edu>
2016-07-22 17:08:41 -0700

    [322. Coin Change][Accepted]committed by LeetCode Extension

--- 322-Coin-Change/solution.java
+++ 322-Coin-Change/solution.java
@@ -3,7 +3,7 @@ public class Solution {
         if(amount<1) return 0;
         //int max = amount + 1;
         int[] dp = new int[amount+1];
-        Arrays.fill(dp, max);
+        Arrays.fill(dp, amount+1);
         dp[0] = 0;
         for(int i=1; i<=amount; i++){
             for(int j=0; j<coins.length; j++){
