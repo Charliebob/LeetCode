9500b9948b684678a388d64eaaf3c886675db736
Charliebob <cxz131330@utdallas.edu>
2016-07-23 11:29:00 -0700

    [336. Palindrome Pairs][Accepted]committed by LeetCode Extension

--- 336-Palindrome-Pairs/solution.java
+++ 336-Palindrome-Pairs/solution.java
@@ -34,4 +34,10 @@ public class Solution {
         }
         return ret;
     }
+    private boolean isPalindrome(String s) {
+        for (int i = 0; i < s.length()/2; ++ i)
+            if (s.charAt(i) != s.charAt(s.length()-1-i))
+                return false;
+        return true;
+    }
 }
