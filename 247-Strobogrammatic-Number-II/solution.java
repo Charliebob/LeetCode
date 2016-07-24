5fcea0cd8971c1eca837c3a80210d8b1b9d269ff
Charliebob <cxz131330@utdallas.edu>
2016-07-24 11:26:20 -0700

    [247. Strobogrammatic Number II][Accepted]committed by LeetCode Extension

--- 247-Strobogrammatic-Number-II/solution.java
+++ 247-Strobogrammatic-Number-II/solution.java
@@ -5,7 +5,7 @@ public class Solution {
     private List<String> helper(int n, int m){
         if(n==0) return new ArrayList<String>(Arrays.asList(""));
         if(n==1) return new ArrayList<String>(Arrays.asList("0","1","8"));
-        List<String> list = helper(n-2, m);
+        List<String> list = helper(n-2, m); //get res if recursion
         List<String> res = new ArrayList<String>();
         for(int i=0; i<list.size(); i++){
             String s= list.get(i);
