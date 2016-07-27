0486572dad30af645fb45547477d954d3a9c62da
Charliebob <cxz131330@utdallas.edu>
2016-07-26 23:32:19 -0700

    [128. Longest Consecutive Sequence][Accepted]committed by LeetCode Extension

--- 128-Longest-Consecutive-Sequence/solution.java
+++ 128-Longest-Consecutive-Sequence/solution.java
@@ -1,24 +1,59 @@
 public class Solution {
     public int longestConsecutive(int[] nums) {
-        int T[] = new int[nums.length];
-        //int actualSolution[] = new int[arr.length];
-        for(int i=0; i < nums.length; i++){
-            T[i] = 1;
-            //actualSolution[i] = i;
-        }
-        
-        for(int i=1; i < nums.length; i++){
-            for(int j=0; j < i; j++){
-                if(nums[i] > nums[j]){
-                    T[i] = Math.max(T[i], T[j]+1);
-                    // if(T[j] + 1 > T[i]){
-                    //     T[i] = T[j] + 1;
-                    //     //set the actualSolution to point to guy before me
-                    //     actualSolution[i] = j;
-                    // }
+        UF uf = new UF(nums.length);
+            Map<Integer,Integer> map = new HashMap<Integer,Integer>(); // <value,index>
+            for(int i=0; i<nums.length; i++){
+                if(map.containsKey(nums[i])){
+                    continue;
+                }
+                map.put(nums[i],i);
+                if(map.containsKey(nums[i]+1)){
+                    uf.union(i,map.get(nums[i]+1));
+                }
+                if(map.containsKey(nums[i]-1)){
+                    uf.union(i,map.get(nums[i]-1));
                 }
             }
+            return uf.maxUnion();
         }
-        return T[nums.length];
+    }
+    
+class UF{
+    private int[] list;
+    public UF(int n){
+        list = new int[n];
+        for(int i=0; i<n; i++){
+            list[i] = i;
+        }
+    }
+    
+    private int root(int i){
+        while(i!=list[i]){
+            list[i] = list[list[i]];
+            i = list[i];
+        }
+        return i;
+    }
+    
+    public boolean connected(int i, int j){
+        return root(i) == root(j);
+    }
+    
+    public void union(int p, int q){
+      int i = root(p);
+      int j = root(q);
+      list[i] = j;
+    }
+    
+    // returns the maxium size of union
+    public int maxUnion(){ // O(n)
+        int[] count = new int[list.length];
+        int max = 0;
+        for(int i=0; i<list.length; i++){
+            count[root(i)] ++;
+            max = Math.max(max, count[root(i)]);
+        }
+        return max;
     }
 }
+
