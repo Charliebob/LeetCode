c0885bb6a7def4256b2b7d7e04fdd521a61ece3e
Charliebob <cxz131330@utdallas.edu>
2016-07-26 20:46:49 -0700

    [286. Walls and Gates][Accepted]committed by LeetCode Extension

--- 286-Walls-and-Gates/solution.java
+++ 286-Walls-and-Gates/solution.java
@@ -1,31 +1,19 @@
 public class Solution {
     public void wallsAndGates(int[][] rooms) {
-        if (rooms.length == 0 || rooms[0].length == 0) return;
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < rooms.length; i++) {
-            for (int j = 0; j < rooms[0].length; j++) {
-                if (rooms[i][j] == 0) queue.add(new int[]{i, j});
+        for(int i = 0; i < rooms.length; i++) {
+            for(int j = 0; j < rooms[0].length; j++) {
+                if(rooms[i][j] == 0) dfs(rooms, i, j, 0);
             }
         }
-        while (!queue.isEmpty()) {
-            int[] top = queue.remove();
-            int row = top[0], col = top[1]; //
-            if (row > 0 && rooms[row - 1][col] == Integer.MAX_VALUE) {
-                rooms[row - 1][col] = rooms[row][col] + 1;
-                queue.add(new int[]{row - 1, col});
-            }
-            if (row < rooms.length - 1 && rooms[row + 1][col] == Integer.MAX_VALUE) {
-                rooms[row + 1][col] = rooms[row][col] + 1;
-                queue.add(new int[]{row + 1, col});
-            }
-            if (col > 0 && rooms[row][col - 1] == Integer.MAX_VALUE) {
-                rooms[row][col - 1] = rooms[row][col] + 1;
-                queue.add(new int[]{row, col - 1});
-            }
-            if (col < rooms[0].length - 1 && rooms[row][col + 1] == Integer.MAX_VALUE) {
-                rooms[row][col + 1] = rooms[row][col] + 1;
-                queue.add(new int[]{row, col + 1});
-            }
-        }
+    }
+
+    public void dfs(int[][] rooms, int i, int j, int g) {
+        //if rooms[i][j] distance is smaller than previous
+        if(i < 0 || i >= rooms.length || j < 0 || j >= rooms[0].length || rooms[i][j] < g) return;
+        rooms[i][j] = g;
+        dfs(rooms, i - 1, j, g + 1);
+        dfs(rooms, i, j - 1, g + 1);
+        dfs(rooms, i + 1, j, g + 1);
+        dfs(rooms, i, j + 1, g + 1);
     }
 }
