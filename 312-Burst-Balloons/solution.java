dee423ddc7e58ead009ec3d205a6f3783dde225c
Charliebob <cxz131330@utdallas.edu>
2016-07-27 21:17:25 -0700

    [312. Burst Balloons][Accepted]committed by LeetCode Extension

--- 312-Burst-Balloons/solution.java
+++ 312-Burst-Balloons/solution.java
@@ -5,9 +5,9 @@ public class Solution {
         for(int x: nums) num[n++] = x;
         num[0] = num[n++] = 1;
         int[][] dp = new int[n][n];
-        for(int k=1; k<n-1; k++){
-            for(int left = 0; left <n-k+1; left++){
-                int right = left + k + 1;
+        for(int k=2; k<n; k++){
+            for(int left = 0; left <n-k; left++){
+                int right = left + k;
                 for(int i=left+1; i<right; i++){
                     dp[left][right] = Math.max(dp[left][right], num[left]*num[i]*num[right]+dp[left][i]+dp[i][right]);
                 }
