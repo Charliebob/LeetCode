190f16780d3d40e0b817aa67e3cd7376bbf602cc
Charliebob <cxz131330@utdallas.edu>
2016-07-27 18:13:20 -0700

    [312. Burst Balloons][Runtime Error]committed by LeetCode Extension

--- 312-Burst-Balloons/solution.java
+++ 312-Burst-Balloons/solution.java
@@ -1,18 +1,39 @@
 public class Solution {
     public int maxCoins(int[] nums) {
-        int[] num = new int[nums.length+2];
-        int n = 1;
-        for(int x: nums) if(x>0) num[n++] = x;
-        num[0] = num[n++] = 1;
-        int[][] dp = new int[n][n];
-        for(int k=2; k<n; k++){
-            for(int left = 0; left <n-k; left++){
-                int right = left + k;
-                for(int i=left+1; i<right; i++){
-                    dp[left][right] = Math.max(dp[left][right], num[left]*num[i]*num[right]+dp[left][i]+dp[i][right]);
+        int T[][] = new int[nums.length][nums.length];
+
+        for (int len = 1; len <= nums.length; len++) {
+            for (int i = 0; i <= nums.length - len; i++) {
+                int j = i + len - 1;
+                for (int k = i; k <= j; k++) {
+                    //leftValue/rightValue is initially 1. If there is element on
+                    // left/right of k then left/right value will take that value.
+                    int leftValue = 1;
+                    int rightValue = 1;
+                    if (i != 0) {
+                        leftValue = nums[i-1];
+                    }
+                    if (j != nums.length -1) {
+                        rightValue = nums[j+1];
+                    }
+
+                    //before is initially 0. If k is i then before will
+                    //stay 0 otherwise it gets value T[i][k-1]
+                    //after is similarly 0 initially. if k is j then after will
+                    //stay 0 other will get value T[k+1][j]
+                    int before = 0;
+                    int after = 0;
+                    if (i != k) {
+                        before = T[i][k-1];
+                    }
+                    if (j != k) {
+                        after = T[k+1][j];
+                    }
+                    T[i][j] = Math.max(leftValue * nums[k] * rightValue + before + after,
+                            T[i][j]);
                 }
             }
         }
-        return dp[0][n-1];
+        return T[0][nums.length - 1];
     }
 }
