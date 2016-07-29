0d94268bfcec227a02b5022bd2ea14df90ed60fd
Charliebob <cxz131330@utdallas.edu>
2016-07-29 12:09:46 -0700

    [164. Maximum Gap][Accepted]committed by LeetCode Extension

--- 164-Maximum-Gap/solution.java
+++ 164-Maximum-Gap/solution.java
@@ -1,22 +1,22 @@
 public class Solution {
     public int maximumGap(int[] nums) {
-        if (num == null || num.length < 2)
+        if (nums == null || nums.length < 2)
             return 0;
         // get the max and min value of the array
-        int min = num[0];
-        int max = num[0];
-        for (int i:num) {
+        int min = nums[0];
+        int max = nums[0];
+        for (int i:nums) {
             min = Math.min(min, i);
             max = Math.max(max, i);
         }
         // the minimum possibale gap, ceiling of the integer division
-        int gap = (int)Math.ceil((double)(max - min)/(num.length - 1));
-        int[] bucketsMIN = new int[num.length - 1]; // store the min value in that bucket
-        int[] bucketsMAX = new int[num.length - 1]; // store the max value in that bucket
+        int gap = (int)Math.ceil((double)(max - min)/(nums.length - 1));
+        int[] bucketsMIN = new int[nums.length - 1]; // store the min value in that bucket
+        int[] bucketsMAX = new int[nums.length - 1]; // store the max value in that bucket
         Arrays.fill(bucketsMIN, Integer.MAX_VALUE);
         Arrays.fill(bucketsMAX, Integer.MIN_VALUE);
-        // put numbers into buckets
-        for (int i:num) {
+        // put numsbers into buckets
+        for (int i:nums) {
             if (i == min || i == max)
                 continue;
             int idx = (i - min) / gap; // index of the right position in the buckets
@@ -26,7 +26,7 @@ public class Solution {
         // scan the buckets for the max gap
         int maxGap = Integer.MIN_VALUE;
         int previous = min;
-        for (int i = 0; i < num.length - 1; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             if (bucketsMIN[i] == Integer.MAX_VALUE && bucketsMAX[i] == Integer.MIN_VALUE)
                 // empty bucket
                 continue;
