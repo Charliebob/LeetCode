5862b317b205d78d67dce1889412c68ab658ac1b
Charliebob <cxz131330@utdallas.edu>
2016-07-31 20:42:37 -0700

    [378. Kth Smallest Element in a Sorted Matrix][Accepted]committed by LeetCode Extension

--- 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.java
+++ 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.java
@@ -1,34 +1,25 @@
 public class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return 0;
-        }
-        int m = matrix.length;
-        int n = matrix[0].length;
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>() {
-            public int compare(Integer a, Integer b) {
-               return b - a;
-            }
-        });
-        
-        for (int i = 0; i < m; i++) {
-            if (pq.size() == k && matrix[i][0] > pq.peek()) {
-                break;
-            }
-            for (int j = 0; j < n; j++) {
-                if (pq.size() == k) {
-                    if (pq.peek() > matrix[i][j]) {
-                        pq.poll();
-                        pq.offer(matrix[i][j]);
-                    }
-                } else {
-                    pq.offer(matrix[i][j]);
-                }
-            }
-            
-        }
-        return pq.peek();
-        
-    }
+        int n = matrix.length;
+		int left = matrix[0][0], right = matrix[n-1][n-1];
+		
+		while (left < right) {
+			int mid = left + (right-left)/2;
+			int temp = 0;
+			for (int i = 0; i < n; i++) temp += binary_search(matrix[i], n, mid);
+			if (temp < k) left = mid + 1;
+			else right = mid;
+		}
+		return left;
+	}
+	
+	private int binary_search(int[] row,int right,int x){
+	    int left = 0;
+	    while (left < right){
+	        int mid = left + (right-left)/2;
+	        if(row[mid] <= x) left = mid + 1;
+	        else right = mid;
+	    }
+	    return left;
+	}
 }
