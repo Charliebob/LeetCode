03a3aa16142fb9d3d9b38600058e9c832bc5863a
Charliebob <cxz131330@utdallas.edu>
2016-08-03 20:00:11 -0700

    [380. Insert Delete GetRandom O(1)][Accepted]committed by LeetCode Extension

--- 380-Insert-Delete-GetRandom-O(1)/solution.java
+++ 380-Insert-Delete-GetRandom-O(1)/solution.java
@@ -1,34 +1,49 @@
 public class RandomizedSet {
 
     /** Initialize your data structure here. */
-    HashMap<Integer, Integer> map;
+    HashMap<Integer,Integer> map;
+    int [] array;
+    int cur;
+    /** Initialize your data structure here. */
     public RandomizedSet() {
-        map = new HashMap<Integer, Integer>();
+        map = new HashMap<>();
+        array = new int [100];
+        cur = 0;
     }
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
-        if(!map.containsKey(val)){
-            map.put(val, 1);
-            return true;
+        if(cur == array.length){
+            int size = 2 * array.length;
+            int [] temp = new int [size];
+            for(int i = 0; i < array.length; i++)
+                temp[i] = array[i];
+            array = temp;
         }
-        return false;
+        if(map.containsKey(val))
+            return false;
+        array[cur++] = val;
+        map.put(val, cur - 1);
+        return true;
     }
     
     /** Deletes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        if(!map.containsKey(val)){
-            map.remove((Integer)val);
-            return true;
-        }
-        return false;
+        if(!map.containsKey(val))
+            return false;
+        int index = map.get(val);
+        int lastIndex = --cur;
+        if(lastIndex >= 0)
+            array[index] = array[lastIndex];
+        map.put(array[lastIndex], index);
+        map.remove(val);
+        return true;
     }
     
     /** Get a random element from the set. */
     public int getRandom() {
-        int index = (int)Math.random()*map.size();
-        
-        return map.get(index);
+        int index = (int)Math.random() * cur;
+        return array[index];
     }
 }
 
