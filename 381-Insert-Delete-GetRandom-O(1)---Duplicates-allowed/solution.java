8c58ed137f4b637405bf2c4be13ee69fbc6fde97
Charliebob <cxz131330@utdallas.edu>
2016-08-09 09:46:49 -0700

    [381. Insert Delete GetRandom O(1) - Duplicates allowed][Accepted]committed by LeetCode Extension

--- 381-Insert-Delete-GetRandom-O(1)---Duplicates-allowed/solution.java
+++ 381-Insert-Delete-GetRandom-O(1)---Duplicates-allowed/solution.java
@@ -1,51 +1,54 @@
-import java.util.Random;
 public class RandomizedCollection {
 
     /** Initialize your data structure here. */
-    private final Random rand;
-    private final Map<Integer,Queue<Integer>> map;
-    private final List<Integer> list;
+    private Map<Integer, Set<Integer>> map;
+    private List<Integer> nums;
+    private java.util.Random random;
+
     /** Initialize your data structure here. */
     public RandomizedCollection() {
-        rand = new Random();
         map = new HashMap<>();
-        list = new ArrayList<>();
+        nums = new ArrayList<>();
+        random = new java.util.Random();
     }
     
     /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
     public boolean insert(int val) {
-        list.add(val);
-        boolean contains = true;
-        if(!map.containsKey(val)){
-            map.put(val, new LinkedList<Integer>());
-            contains = false;
+        boolean res = false;
+        if (!map.containsKey(val)) {
+            res = true;
+            map.put(val, new HashSet<>());
         }
-        map.get(val).offer(list.size()-1); // this new element maps to this index
-        return contains;
+        map.get(val).add(nums.size());
+        nums.add(val);
+        return res;
     }
     
     /** Removes a value from the collection. Returns true if the collection contained the specified element. */
     public boolean remove(int val) {
-        if(!map.containsKey(val)) return false; // this element does not exist
-        Queue<Integer> q = map.get(val);
-        int index = q.poll();
-        if(q.isEmpty()) map.remove(val); // we're removing the only element
-        
-        if(index!=list.size()-1){ // we're removing an element that is not last
-            int lastElement = list.get(list.size()-1);
-            Queue<Integer> lastQueue = map.get(lastElement); // get's the last queue
-            lastQueue.poll();
-            
-            lastQueue.offer(index); // that element now maps to our current element's location
-            list.set(index,lastElement); // set that element in our current position
+        if (!map.containsKey(val)) {
+            return false;
         }
-        list.remove(list.size()-1);
+        
+        Set<Integer> indexs = map.get(val);
+        int index = indexs.iterator().next();
+        if (indexs.size() == 1) {
+            map.remove(val);
+        } else {
+            indexs.remove(index);
+        }
+        if (index < nums.size() - 1) {
+            nums.set(index, nums.get(nums.size() - 1));
+            map.get(nums.get(nums.size() - 1)).remove(nums.size() - 1);
+            map.get(nums.get(nums.size() - 1)).add(index);
+        }
+        nums.remove(nums.size() - 1);  
         return true;
     }
     
     /** Get a random element from the collection. */
     public int getRandom() {
-        return list.get(rand.nextInt(list.size()));
+        return nums.get(random.nextInt(nums.size()));
     }
 }
 
