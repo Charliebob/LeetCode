7d05bd13703c8eb20fca650edffb3bfce97642f1
Charliebob <cxz131330@utdallas.edu>
2016-08-22 14:51:55 -0700

    [386. Lexicographical Numbers][Compile Error]committed by LeetCode Extension

---
+++ 386-Lexicographical-Numbers/solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        if(curr > n){//curr is the number
+            return;
+        }
+        ret.add(curr);
+        for(int i = 0; i < 10; i++){//append 0-9 to the end of curr 
+            if(curr*10 + i <= n){//recurse as long as its less than n
+                solve(curr*10 + i, n, ret);
+            } else break;
+        }
+    }
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> ret = new ArrayList<Integer>();
+        for(int i = 1; i < 10; i++){//fix first digit
+            solve(i, n, ret);
+        }
+        return ret;
+    }
+}
