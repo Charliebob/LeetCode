12bd631603e5a3c10704fa1af9b50f8344229f16
Charliebob <cxz131330@utdallas.edu>
2016-09-04 23:24:53 -0700

    [392. Is Subsequence][Accepted]committed by LeetCode Extension

--- 392-Is-Subsequence/solution.java
+++ 392-Is-Subsequence/solution.java
@@ -4,8 +4,10 @@ public class Solution {
         while(p1<s.length() && p2<t.length()){
             if(s.charAt(p1)==t.charAt(p2)){
                 p1++;
+                p2++;
+            }else{
+                p2++;
             }
-            p2++;
         }
         return p1==s.length();
     }
