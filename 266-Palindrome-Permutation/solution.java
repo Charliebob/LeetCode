1ebd65cc76c6d1314617fa782246cde9fa10da9d
Charliebob <cxz131330@utdallas.edu>
2016-09-05 12:07:11 -0700

    [266. Palindrome Permutation][Compile Error]committed by LeetCode Extension

--- 266-Palindrome-Permutation/solution.java
+++ 266-Palindrome-Permutation/solution.java
@@ -2,12 +2,12 @@ public class Solution {
     public boolean canPermutePalindrome(String s) {
         Set<Character> hs = new HashSet<Character>();
         for(int i=0; i<s.length(); i++){
-            if(hs.contains(s.charAt(i))){
+            if(hs.contains(s.charAt(i)){
                 hs.remove(s.charAt(i));
             }else{
                 hs.add(s.charAt(i));
             }
         }
-        return hs.size()==0 || hs.size()==1;
+        return hs.size()==0||hs.size()==1;
     }
 }
