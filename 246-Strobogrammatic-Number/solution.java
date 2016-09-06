ac9519e136356cf0f86a5be6e864323bbf90b60f
Charliebob <cxz131330@utdallas.edu>
2016-09-06 15:00:01 -0700

    [246. Strobogrammatic Number][Accepted]committed by LeetCode Extension

--- 246-Strobogrammatic-Number/solution.java
+++ 246-Strobogrammatic-Number/solution.java
@@ -1,19 +1,11 @@
 public class Solution {
     public boolean isStrobogrammatic(String num) {
-        Map<Character, Character> map = new HashMap<Character, Character>();
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        map.put('0','0');
+        map.put('1','1');
         map.put('6','9');
-        map.put('9', '6');
-        map.put('0', '0');
-        map.put('1', '1');
-        map.put('8', '8');
-        // int left =0, right = num.length()-1;
-        // while(left<=right){
-        //     if(!map.containsKey(num.charAt(left))) return false;
-        //     if(map.get(num.charAt(left))!= num.charAt(right))
-        //         return false;
-        //     left++;
-        //     right--;
-        // }
+        map.put('8','8');
+        map.put('9','6');
         for(int i=0; i<num.length(); i++){
             if(!map.containsKey(num.charAt(i))) return false;
             if(map.get(num.charAt(i))!=num.charAt(num.length()-1-i)) return false;
