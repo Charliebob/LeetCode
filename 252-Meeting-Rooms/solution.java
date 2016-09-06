ccb650fe9fb58f8fe208c11e173f65453e4087c5
Charliebob <cxz131330@utdallas.edu>
2016-09-05 18:02:02 -0700

    [252. Meeting Rooms][Accepted]committed by LeetCode Extension

--- 252-Meeting-Rooms/solution.java
+++ 252-Meeting-Rooms/solution.java
@@ -11,14 +11,16 @@ public class Solution {
     public boolean canAttendMeetings(Interval[] intervals) {
         if(intervals==null) return false;
         Arrays.sort(intervals, new Comparator<Interval>(){
-            public int compare(Interval a, Interval b){
-                return a.start-b.start;
+            @Override
+            public int compare(Interval i1, Interval i2){
+                return i1.start-i2.start;
             }
         });
         for(int i=1; i<intervals.length; i++){
-            if(intervals[i].start<intervals[i-1].end) return false;
+            if(intervals[i].start<intervals[i-1].end){
+                return false;
+            }
         }
         return true;
     }
 }
-
