0da0675d7ed383de432b8d2442dbcba3537f3b5c
Charliebob <cxz131330@utdallas.edu>
2016-09-11 17:26:51 -0700

    [276. Paint Fence][Accepted]committed by LeetCode Extension

--- 276-Paint-Fence/solution.java
+++ 276-Paint-Fence/solution.java
@@ -9,7 +9,7 @@ public class Solution {
         same[0] = same[1] = k;
         diff[0] = k;
         diff[1] = k * (k - 1);
-        for (int i = 2; i < n; ++i) {
+        for (int i = 2; i < n; i++) {
             same[i] = diff[i-1];
             diff[i] = (k - 1) * same[i-1] + (k - 1) * diff[i-1];
         }
@@ -18,4 +18,11 @@ public class Solution {
 }
 
 
-
+// if(n==0||k==0) return 0;
+// if(n==1) return k;
+// int[] same = new int[n];
+// int[] diff = new int[n];
+// same[0] = same[1] = k;
+// diff[0] = k;
+// diff[1] = k*(k-1);
+// for(int i=2; i<n; ++i)
