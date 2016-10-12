8dc39a53b1b3aae25e9f5a854d4918a7a1c7b04b
Charliebob <cxz131330@utdallas.edu>
2016-10-12 10:05:53 -0700

    [416. Partition Equal Subset Sum][Compile Error]committed by LeetCode Extension

---
+++ 416-Partition-Equal-Subset-Sum/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean canPartition(int[] nums) {
+        if (nums == null || nums.length == 0) return true;
+        int sum = 0;
+        for(int i: nums){
+            sum+=i;
+        }
+        if(sum%2!=0) return false;
+        sum/=2;
+        boolean[] dp = new boolean[sum+1];
+        dp[0] = true;
+        for(int i=1; i<nums.length; i++){
+            for(int j=volumn; j>=nums[i-1]; j--){
+                dp[j] = dp[j] || dp[j-nums[i-1]];
+            }
+        }
+        return dp[sum];
+    }
+}
